c4aec312665e70213404168f4361716dbb7ee512
D-Perez85 <damianperez22@hotmail.com>
2024-02-27 07:51:51 -0300

    Add de Card y Form

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <h3>Soy un componente Card</h3>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <h4>Soy un Formulario</h4>
+    </div>
+  )
+}
+
+export default Form
